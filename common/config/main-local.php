bc815839e1a1d4876e93147e0d1f93bd5f9df23f
shongpon <shongpon@gmail.com>
2015-05-15 14:39:56 +0700

    เพิ่มไฟล์ Config

---
+++ common/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=yii2advanced',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'db_sko' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=203.157.145.16;dbname=a6',
+            'username' => 'childdev',
+            'password' => 'childdevSakaeo',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
