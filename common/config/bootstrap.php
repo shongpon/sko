cfd57df39de7ae7365bd8ccf23805d7dc8db22b5
shongpon <shongpon@gmail.com>
2015-05-15 09:41:31 +0700

    Saraban folder commit

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('saraban', dirname(dirname(__DIR__)) . '/saraban');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
